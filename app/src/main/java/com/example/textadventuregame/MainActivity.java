8e02f99ff03314dd28f01b0e10a3e37382a24376
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-15 14:18:21 +0200

    help activity

--- app/src/main/java/com/example/textadventuregame/MainActivity.java
+++ app/src/main/java/com/example/textadventuregame/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.textadventuregame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button helpButton;
+    Button aboutButton;
+    Button playButton;
+    Button loadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupControls();
+    }
+
+    private void setupControls() {
+        helpButton = findViewById(R.id.buttonHelp);
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
