d438b161053f409d8122516037ae9ffcc565a1cf
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-17 21:40:35 +0200

    display rooms test

--- app/src/main/java/com/example/textadventuregame/PlayActivity.java
+++ app/src/main/java/com/example/textadventuregame/PlayActivity.java
@@ -3,13 +3,42 @@ package com.example.textadventuregame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class PlayActivity extends AppCompatActivity {
 
-    // public class Room
+    static final int NUM_OF_ROOMS = 10; // Setup the number of rooms of the app
+    Room [] thedungeon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-    }
-}
+
+        initDungeon();
+        displayRooms();
+    } // protected void onCreate
+
+    protected void initDungeon() {
+        thedungeon = new Room[NUM_OF_ROOMS];
+        for (int pos = 0; pos < NUM_OF_ROOMS; pos++)
+        {
+            thedungeon[pos] = new Room();
+        }
+    } // public static void initDungeon()
+
+    private void displayRooms() {
+        Log.w("display ROOM", "**** start of display rooms ****");
+
+        for (int pos = 0; pos < NUM_OF_ROOMS; pos++)
+        {
+            Log.w("display ROOM", "North = " + thedungeon[pos].getNorth());
+            Log.w("display ROOM", "East = " + thedungeon[pos].getEast());
+            Log.w("display ROOM", "West = " + thedungeon[pos].getWest());
+            Log.w("display ROOM", "South = " + thedungeon[pos].getSouth());
+            Log.w("display ROOM", "Description = " + thedungeon[pos].getDescription());
+        }
+        Log.w("display ROOM", "**** end of display rooms ****");
+    } // public void displayRooms()
+
+} // public class PlayActivity
--- app/src/main/java/com/example/textadventuregame/StartActivity.java
+++ app/src/main/java/com/example/textadventuregame/StartActivity.java
@@ -2,6 +2,7 @@ package com.example.textadventuregame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,15 @@ public class StartActivity extends AppCompatActivity {
     private void setupControls() {
 
         // Navigation
+        nextButton = findViewById(R.id.buttonNext);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
         backButton = findViewById(R.id.buttonStartBack);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
