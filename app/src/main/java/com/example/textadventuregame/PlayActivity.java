cf2de9c4ff3191b5383d27d849112f54ee42a94d
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-17 19:49:09 +0200

    update dingeons

---
+++ app/src/main/java/com/example/textadventuregame/PlayActivity.java
@@ -0,0 +1,15 @@
+package com.example.textadventuregame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayActivity extends AppCompatActivity {
+
+    // public class Room
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
