cfc91f5517ea84d459dbd2dc7980cab938473b36
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-19 22:25:20 +0200

    display choices

--- app/src/main/java/com/example/textadventuregame/FightActivity.java
+++ app/src/main/java/com/example/textadventuregame/FightActivity.java
@@ -27,7 +27,7 @@ public class FightActivity extends AppCompatActivity {
     ImageView playerAttackImage;
     ImageView enemyAttackImage;
 
-    enum Choice {LIGHT, SHIELD, HARD, NONE};
+    enum Choice {LIGHT, SHIELD, HEAVY, NONE};
     Choice playerChoice = Choice.NONE;
     Choice enemyChoice = Choice.NONE;
 
@@ -63,9 +63,66 @@ public class FightActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 playerChoice = Choice.LIGHT;
-                playerAttackImage.setImageResource(R.drawable.sworduser);
+                playerAttackImage.setImageResource(R.drawable.sworduser); // We change the path of the image if clicked
+                playerAttackImage.setVisibility(View.VISIBLE); // We define that the image is visible
+
+                enemyChoice();
             }
         });
 
-    }
+        heavyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = Choice.HEAVY;
+                playerAttackImage.setImageResource(R.drawable.heavysword); // We change the path of the image if clicked
+                playerAttackImage.setVisibility(View.VISIBLE); // We define that the image is visible
+
+                enemyChoice();
+            }
+        });
+
+        shieldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerChoice = Choice.SHIELD;
+                playerAttackImage.setImageResource(R.drawable.shield); // We change the path of the image if clicked
+                playerAttackImage.setVisibility(View.VISIBLE); // We define that the image is visible
+
+                enemyChoice();
+            }
+        });
+    } // private void setupControls()
+
+    private void enemyChoice() {
+        // We define the attack choice of the enemy
+        int choice = 0;
+        choice = generateRandomNumber();
+
+        if (choice == 0){
+            enemyChoice = Choice.LIGHT;
+            enemyAttackImage.setImageResource(R.drawable.swordennemi);
+            enemyAttackImage.setVisibility(View.VISIBLE);
+        }
+        else if (choice == 1){
+            enemyChoice = Choice.SHIELD;
+            enemyAttackImage.setImageResource(R.drawable.shield_ennemy);
+            enemyAttackImage.setVisibility(View.VISIBLE);
+        }
+        else if (choice == 2){
+            enemyChoice = Choice.HEAVY;
+            enemyAttackImage.setImageResource(R.drawable.heavyswordennemy);
+        }
+        checkWin();
+    } // private void EnemyChoice()
+
+    private int generateRandomNumber() {
+        // We define a random number
+        random = new Random();
+        return random.nextInt(3);
+    } // private int enemyChoice()
+
+    private void checkWin(){
+
+    } // private void checkWin()
+
 }
