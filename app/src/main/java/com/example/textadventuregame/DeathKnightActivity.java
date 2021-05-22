36cbfc1a8e6b311bce999f7236491e11061af92f
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-22 13:14:14 +0200

    gif

--- app/src/main/java/com/example/textadventuregame/DeathKnightActivity.java
+++ app/src/main/java/com/example/textadventuregame/DeathKnightActivity.java
@@ -3,6 +3,7 @@ package com.example.textadventuregame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +21,18 @@ public class DeathKnightActivity extends AppCompatActivity {
     }
 
     private void setupControls() {
+        // Music
+        MediaPlayer ring = MediaPlayer.create(DeathKnightActivity.this, R.raw.death);
+        ring.setLooping(true); // We loop the music
+        ring.start();
+
         backButton = findViewById(R.id.BackMainMenuButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(intent);
+                ring.stop();
             }
         });
     }
